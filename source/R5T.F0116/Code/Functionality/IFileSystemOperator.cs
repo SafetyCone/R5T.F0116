b566bd8b5b9c67a7f86e2876422dd3d2d2cc512d
davidcoats <david@getsafetycone.com>
2023-03-23 02:29:15 -0700

    Interim changes.

---
+++ source/R5T.F0116/Code/Functionality/IFileSystemOperator.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using R5T.F0000;
+using R5T.T0132;
+using R5T.T0175;
+using R5T.T0175.Extensions;
+
+
+namespace R5T.F0116
+{
+    [FunctionalityMarker]
+    public partial interface IFileSystemOperator : IFunctionalityMarker,
+        F0000.IFileSystemOperator
+    {
+        public IEnumerable<(DirectoryPath, T)> EnumerateChildDirectories_OfType<T>(
+            IDirectoryPath directoryPath,
+            Func<IDirectoryName, WasFound<T>> directoryNameDiscriminator)
+        {
+            var childDirectoryPaths = this.EnumerateAllChildDirectoryPaths(directoryPath.Value);
+
+            var priorDirectoryDates = childDirectoryPaths
+               .Select(directoryPath => new { DirectoryPath = directoryPath.ToDirectoryPath(), DirectoryName = Instances.PathOperator.GetDirectoryName(directoryPath).ToDirectoryName() })
+               .Select(pair => new { pair.DirectoryPath, WasFound = directoryNameDiscriminator(pair.DirectoryName) })
+               .Where(pair => pair.WasFound)
+               .Select(pair => (pair.DirectoryPath, pair.WasFound.Result))
+               ;
+
+            return priorDirectoryDates;
+        }
+
+        public IEnumerable<(DirectoryPath DirectoryPath, DateTime DirectoryDateTime)> EnumerateChildDirectories_YYYYMMDD(
+            IDirectoryPath directoryPath)
+        {
+            var priorDirectoryDates = this.EnumerateChildDirectories_OfType(
+                directoryPath,
+                directoryName => Instances.DateTimeOperator.Is_YYYYMMDD(directoryName.Value));
+
+            return priorDirectoryDates;
+        }
+
+        public IEnumerable<(DirectoryPath DirectoryPath, DateTime DirectoryDateTime)> EnumerateChildDirectories_YYYYMMDD_HHMMSS(
+            IDirectoryPath directoryPath)
+        {
+            var priorDirectoryDates = this.EnumerateChildDirectories_OfType(
+                directoryPath,
+                directoryName => Instances.DateTimeOperator.Is_YYYYMMDD_HHMMSS(directoryName.Value));
+
+            return priorDirectoryDates;
+        }
+    }
+}
--- source/R5T.F0116/Code/Instances.cs
+++ source/R5T.F0116/Code/Instances.cs
@@ -5,6 +5,7 @@ namespace R5T.F0116
 {
     public static class Instances
     {
-		
+        public static F0000.IDateTimeOperator DateTimeOperator => F0000.DateTimeOperator.Instance;
+        public static F0002.IPathOperator PathOperator => F0002.PathOperator.Instance;
     }
 }
